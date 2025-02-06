55206691d192fd4a6ce255d83a541b44474b4083
Alin Zamfiroiu <alinz10@yahoo.com>
2025-02-06 16:00:38 +0200

    Add project files.

---
+++ Sablon02.c
@@ -0,0 +1,57 @@
+//#include<stdio.h>
+//#include<malloc.h>
+//
+//struct Sablon {
+//	int id;
+//	//int ;
+//	//char* ;
+//	//float ;
+//	//char ;
+//};
+//struct Sablon initializare(/*parametri necesari*/) {
+//	struct Sablon s;
+//	s.id = 1;
+//	//initializare structura 
+//	return s;
+//}
+//
+//void afisare(struct Sablon s) {
+//	//afisarea tuturor atributelor.
+//}
+//
+//void afisareVector(struct Sablon* vector, int nrElemente) {
+//	//afisarea elementelor din vector apeland functia afisare
+//}
+//
+//struct Sablon* copiazaPrimeleNElemente(struct Sablon* vector, int nrElemente, int nrElementeCopiate) {
+//	//copiem intr-un vector nou pe care il vom returna primele nrElementeCopiate
+//	struct Sablon *vectorNou=NULL;
+//
+//	return vectorNou;
+//}
+//
+//void dezalocare(struct Sablon** vector, int* nrElemente) {
+//	//dezalocam elementele din vector si vectorul
+//}
+//
+//void copiazaAnumiteElemente(struct Sablon* vector, char nrElemente, float prag, struct Sablon** vectorNou, int* dimensiune) {
+//	//parametrul prag poate fi modificat in functie de 
+//	// tipul atributului ales pentru a indeplini o conditie
+//	//este creat un nou vector cu elementele care indeplinesc acea conditie
+//}
+//
+//struct Sablon getPrimulElementConditionat(struct Sablon* vector, int nrElemente, const char* conditie) {
+//	//trebuie cautat elementul care indeplineste o conditie
+//	//dupa atributul de tip char*. Acesta este returnat.
+//	struct Sablon s;
+//	s.id = 1;
+//
+//	return s;
+//}
+//	
+//
+//
+//int main() {
+//
+//	return 0;
+//}
