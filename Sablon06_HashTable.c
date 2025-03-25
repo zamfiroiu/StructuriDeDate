ba9edba21626ce844854869a2be3dbfe9e9ff90e
Alin Zamfiroiu <alinz10@yahoo.com>
2025-03-25 09:26:18 +0200

    Sablon 6 - Hash Table

---
+++ Sablon06_HashTable.c
@@ -0,0 +1,117 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//
+////trebuie sa folositi fisierul masini.txt
+////sau va creati un alt fisier cu alte date
+//
+//struct StructuraMasina {
+//	int id;
+//	int nrUsi;
+//	float pret;
+//	char* model;
+//	char* numeSofer;
+//	unsigned char serie;
+//};
+//typedef struct StructuraMasina Masina;
+//
+////creare structura pentru un nod dintr-o lista simplu inlantuita
+//
+////creare structura pentru tabela de dispersie
+//// aceasta este un vector de liste
+//struct HashTable {
+//	int dim;
+//};
+//typedef struct HashTable HashTable;
+//
+//Masina citireMasinaDinFisier(FILE* file) {
+//	char buffer[100];
+//	char sep[3] = ",\n";
+//	fgets(buffer, 100, file);
+//	char* aux;
+//	Masina m1;
+//	aux = strtok(buffer, sep);
+//	m1.id = atoi(aux);
+//	m1.nrUsi = atoi(strtok(NULL, sep));
+//	m1.pret = atof(strtok(NULL, sep));
+//	aux = strtok(NULL, sep);
+//	m1.model = malloc(strlen(aux) + 1);
+//	strcpy_s(m1.model, strlen(aux) + 1, aux);
+//
+//	aux = strtok(NULL, sep);
+//	m1.numeSofer = malloc(strlen(aux) + 1);
+//	strcpy_s(m1.numeSofer, strlen(aux) + 1, aux);
+//
+//	m1.serie = *strtok(NULL, sep);
+//	return m1;
+//}
+//
+//void afisareMasina(Masina masina) {
+//	printf("Id: %d\n", masina.id);
+//	printf("Nr. usi : %d\n", masina.nrUsi);
+//	printf("Pret: %.2f\n", masina.pret);
+//	printf("Model: %s\n", masina.model);
+//	printf("Nume sofer: %s\n", masina.numeSofer);
+//	printf("Serie: %c\n\n", masina.serie);
+//}
+//
+//void afisareListaMasini(/*lista de masini*/) {
+//	//afiseaza toate elemente de tip masina din lista dublu inlantuita
+//	//prin apelarea functiei afisareMasina()
+//}
+//
+//void adaugaMasinaInLista(/*lista de masini*/ Masina masinaNoua) {
+//	//adauga la final in lista primita o noua masina pe care o primim ca parametru
+//}
+//
+//
+//HashTable initializareHashTable(int dimensiune) {
+//	HashTable ht;
+//	//initializeaza vectorul de liste si seteaza fiecare lista ca fiind NULL;
+//	return ht;
+//}
+//
+//int calculeazaHash(/*atribut al masini pentru clusterizare*/ int dimensiune) {
+//	// este calculat hash-ul in functie de dimensiunea tabelei si un atribut al masinii
+//}
+//
+//void inserareMasinaInTabela(HashTable hash, Masina galerie) {
+//	//este folosit mecanismul CHAINING
+//	//este determinata pozitia si se realizeaza inserarea pe pozitia respectiva
+//}
+//
+//HashTable citireMasiniDinFisier(const char* numeFisier) {
+//	//functia primeste numele fisierului, il deschide si citeste toate masinile din fisier
+//	//prin apelul repetat al functiei citireMasinaDinFisier()
+//	// aceste masini sunt inserate intr-o tabela de dispersie initializata aici
+//	//ATENTIE - la final inchidem fisierul/stream-ul
+//}
+//
+//void afisareTabelaDeMasini(HashTable ht) {
+//	//sunt afisate toate masinile cu evidentierea clusterelor realizate
+//}
+//
+//void dezalocareTabelaDeMasini(HashTable *ht) {
+//	//sunt dezalocate toate masinile din tabela de dispersie
+//}
+//
+//float* calculeazaPreturiMediiPerClustere(HashTable ht, int* nrClustere) {
+//	//calculeaza pretul mediu al masinilor din fiecare cluster.
+//	//trebuie sa returnam un vector cu valorile medii per cluster.
+//	//lungimea vectorului este data de numarul de clustere care contin masini
+//	return NULL;
+//}
+//
+//Masina getMasinaDupaCheie(HashTable ht /*valoarea pentru masina cautata*/) {
+//	Masina m;
+//	//cauta masina dupa valoarea atributului cheie folosit in calcularea hash-ului
+//	//trebuie sa modificam numele functiei 
+//	return m;
+//}
+//
+//int main() {
+//
+//
+//	return 0;
+//}
